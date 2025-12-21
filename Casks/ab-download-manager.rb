84de6fd7e6c9c4763cdce3ea1a7996d0f5566517
CoZo <2606767372@qq.com>
2025-12-21 16:28:19 +0800

    Update

--- Casks/ab-download-manager.rb
+++ Casks/ab-download-manager.rb
@@ -1,20 +1,66 @@
 cask "ab-download-manager" do
-  arch arm: "arm64", intel: "x64"
+  on_macos do
+    arch arm: "arm64", intel: "x64"
 
-  version "1.8.1"
-  sha256 arm:   "053920bd7a9baedf1f745811d46ce43bf30555d68f1a4765785a308031f6063b",
-         intel: "3781dfde05a697a6d1dea7014ce05800eb3934746031428e337d365aca5a4b83"
+    version "1.8.1"
+    sha256 arm:   "053920bd7a9baedf1f745811d46ce43bf30555d68f1a4765785a308031f6063b",
+           intel: "3781dfde05a697a6d1dea7014ce05800eb3934746031428e337d365aca5a4b83"
 
-  url "https://github.com/amir1376/ab-download-manager/releases/download/v#{version}/ABDownloadManager_#{version}_mac_#{arch}.dmg",
-      verified: "github.com/amir1376/ab-download-manager/"
-  name "AB Download Manager"
-  desc "Download manager that speeds up your downloads"
-  homepage "https://abdownloadmanager.com/"
+    url "https://github.com/amir1376/ab-download-manager/releases/download/v#{version}/ABDownloadManager_#{version}_mac_#{arch}.dmg",
+        verified: "github.com/amir1376/ab-download-manager/"
+    name "AB Download Manager"
+    desc "Download manager that speeds up your downloads"
+    homepage "https://abdownloadmanager.com/"
 
-  livecheck do
-    url :url
-    strategy :github_latest
+    livecheck do
+      url :url
+      strategy :github_latest
+    end
+
+    auto_updates true
+
+    app "ABDownloadManager.app"
+
+    uninstall_postflight do
+      system_command "/usr/bin/pkill",
+                     args:         ["-f", "ABDownloadManager.app"],
+                     must_succeed: false
+    end
+
+    uninstall quit:       "com.abdownloadmanager.desktop",
+              login_item: "ABDownloadManager",
+              delete:     "~/Library/LaunchAgents/AB Download Manager.plist"
+
+    zap trash: [
+      "~/.abdm",
+      "~/Library/Preferences/com.abdownloadmanager.desktop.desktop.plist",
+    ]
   end
 
-  app "ABDownloadManager.app"
+  on_linux do
+    version "1.8.1"
+    sha256 :no_check
+
+    url "https://raw.githubusercontent.com/amir1376/ab-download-manager/master/scripts/install.sh"
+    name "AB Download Manager"
+    desc "Download manager that speeds up your downloads"
+    homepage "https://abdownloadmanager.com/"
+
+    installer script: {
+      executable: "bash",
+      args:       [
+        "-c",
+        "curl -fsSL https://raw.githubusercontent.com/amir1376/ab-download-manager/master/scripts/install.sh | bash",
+      ],
+    }
+
+    uninstall script: {
+      executable: "bash",
+      args:       [
+        "-c",
+        "curl -fsSL " \
+        "https://raw.githubusercontent.com/amir1376/ab-download-manager/master/scripts/uninstall.sh | bash",
+      ],
+    }
+  end
 end
